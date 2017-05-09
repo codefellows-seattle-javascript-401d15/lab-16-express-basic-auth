f04f37a3ff5135105915df0e6d6f5a0591a6ca62
Steven Johnson <stevenjohnson86@gmail.com>
2017-05-09 12:43:28 -0700

    tests scaffolded, began adding first test

--- lab-steven/test/tests.js
+++ lab-steven/test/tests.js
@@ -1,1 +1,52 @@
 'use strict';
+
+const chai = require('chai');
+const http = require('chai-http');
+const expect = chai.expect;
+const server = require('../server');
+
+chai.use(http);
+
+describe('server', function(){
+  before(function(done){
+    server.listen(3000);
+    done();
+  });
+
+  describe('POST /api/signup', function(){
+    it('responds with 404 for bad routes', done => {
+      chai.request()
+
+      expect()
+      done();
+    });
+    it('responds with 400 status for bad request', done => {
+
+      done();
+    });
+    it('responds 200 for successful signup', done => {
+
+      done();
+    });
+  });
+
+  describe('GET /api/signin', function(){
+    it('responds with 404 for bad routes', done => {
+
+      done();
+    });
+    it('responds with 401 status for failed auth', done => {
+
+      done();
+    });
+    it('responds with 200 for successful signin', done => {
+
+      done();
+    });
+  });
+
+  after(function(done){
+    server.close();
+    done();
+  });
+});
