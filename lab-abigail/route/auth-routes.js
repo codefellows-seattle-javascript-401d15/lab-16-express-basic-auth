e79596d5f9632e83895beb1b4bd9d3f40d9c1c77
Abigail White <abigailwhite213@gmail.com>
2017-05-09 08:42:29 -0700

    initial commit

---
+++ lab-abigail/route/auth-routes.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const debug = require('debug')('cfgram:auth-routes');
+const basicAuth = require('../lib/basic-auth-middleware');
+const User = require('../model/user');
+
+module.exports = function(router) {
+  router.post('/signup', (req, res) => {
+    debug('POST /signup');
+
+    let tempPassword = req.body.password;
+    req.body.password = null;
+    delete req.body.password;
+
+    let newUser = new User(req.body);
+
+    return newUser.generatePasswordHash(tempPassword)
+    .then(user => user.save())
+    .then(user => user.generateToken())
+    .then(token => res.json(token))
+    .catch(err => res.status(err.status).send(err));
+  });
+
+  router.get('/signin', basicAuth, (req, res) => {
+    debug('GET /signin');
+
+    return User.findOne({username: req.auth.username})
+    .then(user => user.comparePasswordHash(req.auth.password))
+    .then(user => user.generateToken())
+    .then(token => res.json(token))
+    .catch(err => res.status(err.status).send(err));
+  });
+};
