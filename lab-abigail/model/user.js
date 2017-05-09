c8ccba10aa72c8ad8dd1cacb9cff20bd24dd6297
Abigail White <abigailwhite213@gmail.com>
2017-05-09 14:40:31 -0700

    change env

--- lab-abigail/model/user.js
+++ lab-abigail/model/user.js
@@ -14,7 +14,7 @@ const userSchema = Schema({
   username: {type: String, required: true, unique: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  findhash: {type: String, unique: true},
+  findHash: {type: String, unique: true},
 });
 
 userSchema.methods.generatePasswordHash = function(password) {
@@ -54,7 +54,7 @@ userSchema.methods.generateFindHash = function() {
       .then(() => resolve(this.findHash))
       .catch(err => {
         console.log(err);
-        if(tries > 3) return reject(createError(401, 'Generate findhash failed'));
+        if(tries > 3) return reject(createError(401, 'Generate findHash failed'));
         tries++;
         _generateFindHash();
       });
