9166f0d46139c683d16bdfa55c6da4a34de5d6ff
Disa <justdisa@gmail.com>
2017-05-10 11:41:29 -0700

    committing to get a clean start

--- disa-lab/controllers/auth-controller.js
+++ disa-lab/controllers/auth-controller.js
@@ -3,4 +3,14 @@
 const debug = require('debug')('cfgram:auth-controller');
 const User = require('../models/user');
 
-module.exports = exports = {}; //assigning an alias
+module.exports = exports = {}; //assigning an alias--storing in an empty object
+
+exports.createNewUser = function(body) {
+  dubug('#createNewUser');
+
+  let tempPassword = body.password;
+  body.password = null;
+  delete body.password;
+
+  let newUser = new User(body);
+};
--- disa-lab/routes/auth-routes.js
+++ disa-lab/routes/auth-routes.js
@@ -1,23 +1,23 @@
 'use strict';//Some parts of this need to go into auth-controller, but I don't know what. still figuring it out.
-
+//routes in router, what it does goes in the controller.
 const debug = require('debug')('cfgram:auth-routes');
 const basicAuth = require('../lib/basic-auth-middleware');
 const User = require('../models/user');
 
 module.exports = function(router) {
   router.post('/signup', (req, res) => {
-    debug('POST /signup');
-
-    let tempPassword = req.body.password;
-    req.body.password = null;
-    delete req.body.password;
+    debug('POST /signup');//debug is "superdope" =D
+      //hashtag is just a flag saying you're inside the thing
+    let tempPassword = req.body.password; //storing temp password
+    req.body.password = null; //
+    delete req.body.password;//
 
     let newUser = new User(req.body);
 
     return newUser.generatePasswordHash(tempPassword)
     .then(user => user.save())
     .then(user => user.generateToken())
-    .then(token => res.json(token))
+    .then(token => res.json(token))//changes to json format
     .catch(err => res.status(err.status).send(err));
   });
 
