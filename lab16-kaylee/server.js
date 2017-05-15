731b2073b033b75e675e525631dcff2b20c8b2c4
Kaylee Alvarado <kaymay1000@gmail.com>
2017-05-15 00:11:46 -0700

    All but one test passing

--- lab16-kaylee/server.js
+++ lab16-kaylee/server.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/auth-routes');
 const bodyParser = require('body-parser').json();
 const mongoose = require('mongoose');
 
-const app = express();
+const app = module.exports = express();
 const router = express.Router();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/doggramDB';
